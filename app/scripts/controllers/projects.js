a249ea3ca7c781fb5302469c027c5486470d6a89
collinbarnwell <steezmeister@gmail.com>
2015-05-27 01:50:04 -0500

    dat hot yung modal be workin, jus needz animashuns

--- app/scripts/controllers/projects.js
+++ app/scripts/controllers/projects.js
@@ -15,17 +15,44 @@ angular.module('epicWwwApp')
         'Karma'
     ];
 
+    $scope.projects = [
+        { name: "WildHacks", image: "../images/wildhacks-logo.png", color: "#f38288" },
+        { name: "NUVC", image: "../images/nuvc.png", color: "#ef5961" },
+        { name: "EPIC Labs", image: "../images/epic-labs-white.png", color: "#b0232b" },
+        { name: "Project Pitch", image: "../images/project-pitch-white.png", color: "#76181d" },
+        // { name: "", image: "../images/epic-labs-white.png", color: "#3b0c0e" }
+    ];
+
     $scope.modalOpen = false;
+    $scope.projNum = 3;
+
+    $scope.currentProjectClass = function(project) {
+        if (project.name == $scope.projects[$scope.projNum].name)
+            return 'current';
+        return '';
+    }
+
+    $scope.isCurrent = function(project) {
+        if (project.name == $scope.projects[$scope.projNum].name)
+            return true;
+        return false;
+    }
+
+    $scope.displayMinus = function(projName) {
+        if ($scope.projNum > 0)
+            $scope.projNum -= 1;
+    };
+
+    $scope.displayPlus = function() {
+        if ($scope.projNum < $scope.projects.length)
+            $scope.projNum += 1;
+    };
 
     $scope.openModal = function() {
-        this.modalOpen = true;
+        $scope.modalOpen = true;
     };
 
     $scope.closeModal = function() {
-        this.modalOpen = false;
+        $scope.modalOpen = false;
     };
-
-    $scope.projects = [
-        { name: "NUVC" }
-    ];
 });
